bb002e505368ad8ea931f9d590e973a4ffc08674
Yan <yanflusantos@gmail.com>
2025-03-29 10:44:42 -0300

    UpdateFunctionDelete

--- stock/static/js/main.js
+++ stock/static/js/main.js
@@ -13,22 +13,27 @@ function openModal(id, name, description, stock, price) {
     document.getElementById("description").value = description;
     document.getElementById("stock").value = stock;
     document.getElementById("price").value = price;
-    
-    console.log(document.getElementById("updateForm").action);
 
     document.getElementById("updateForm").addEventListener("submit", function(event) {
         event.preventDefault();  // Evita o envio imediato
     
         let id = document.getElementById("equipament_id").value;  // Obtém o ID do input
-        // Atualiza a ação do formulário para incluir o ID na URL
-        this.action = `/update/${id}/`;
-    
+        this.action = `/update/${id}/`; // Atualiza a ação do formulário para incluir o ID na URL
         this.submit();  // Agora envia o formulário com a URL correta
     });
 
-    console.log(document.getElementById("updateForm").action);
+    let link = document.getElementById("updateLink");  // Obtém o elemento <a>
+    console.log(link);  // Verifica se o href foi atualizado corretamente
+        // Atualiza a ação do formulário para incluir o ID na URL
+    if (link) {
+        link.href = `/delete/${id}/`;  // Atualiza o href do link
+    }
+
+    
     document.getElementById("modalOverlay").style.display = "flex";
 }
+
+
 
 
 
