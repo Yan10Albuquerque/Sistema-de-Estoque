5826773f3ac4f9a2735d096ffbd674f9608f9015
Yan <yanflusantos@gmail.com>
2025-03-29 17:20:06 -0300

    version 0.0.4

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for core project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
+import sys
 import os
 
+path = "/home/yan-albuquerque/Sistema-de-Estoque"
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
 application = get_wsgi_application()
