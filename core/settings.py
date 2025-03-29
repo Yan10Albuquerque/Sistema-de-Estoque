e2efd82f361def942fcc64f70eb3b5b76bda3cc0
Yan <yanflusantos@gmail.com>
2025-03-29 17:55:26 -0300

    version 0.0.6

--- core/settings.py
+++ core/settings.py
@@ -110,7 +110,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Diretório onde os arquivos estáticos do projeto ficam
+]
 
 
 # Default primary key field type
---
+++ staticfiles/js/main.js
@@ -0,0 +1,46 @@
+function showModal() {
+    document.getElementById('modal').style.display = 'flex';
+}
+
+function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+    document.getElementById('modalOverlay').style.display = 'none';
+}
+
+function openModal(id, name, description, stock, price) {
+    document.getElementById("equipament_id").value = id;
+    document.getElementById("name").value = name;
+    document.getElementById("description").value = description;
+    document.getElementById("stock").value = stock;
+    document.getElementById("price").value = price;
+
+    document.getElementById("updateForm").addEventListener("submit", function(event) {
+        event.preventDefault();  // Evita o envio imediato
+    
+        let id = document.getElementById("equipament_id").value;  // Obtém o ID do input
+        this.action = `/update/${id}/`; // Atualiza a ação do formulário para incluir o ID na URL
+        this.submit();  // Agora envia o formulário com a URL correta
+    });
+
+    let link = document.getElementById("updateLink");  // Obtém o elemento <a>
+    console.log(link);  // Verifica se o href foi atualizado corretamente
+        // Atualiza a ação do formulário para incluir o ID na URL
+    if (link) {
+        link.href = `/delete/${id}/`;  // Atualiza o href do link
+    }
+
+    
+    document.getElementById("modalOverlay").style.display = "flex";
+}
+
+
+
+function filterTable() {
+    let input = document.getElementById("searchInput").value.toLowerCase();
+    let rows = document.querySelectorAll("#table-body tr");
+
+    rows.forEach(row => {
+        let text = row.textContent.toLowerCase();
+        row.style.display = text.includes(input) ? "" : "none";
+    });
+}
