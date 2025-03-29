fde661e3a2a7fc840dc2e972e762afb25134b130
Yan <yanflusantos@gmail.com>
2025-03-29 17:30:13 -0300

    version 0.0.4

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,12 +1,10 @@
 
-import sys
 import os
 
-path = "/home/yan-albuquerque/Sistema-de-Estoque"
-if path not in sys.path:
-    sys.path.append(path)
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
-
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
 application = get_wsgi_application()
+
+
